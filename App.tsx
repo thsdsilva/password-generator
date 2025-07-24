dc7c005fec17c3dbd29f5fcb919574f22639f6f3
thsdsilva <thsds@pm.me>
2025-07-23 22:20:35 -0300

    Refactor App component to display logo image and update styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+<View style={ styles.container }>
+    <Image
+      source={require("./src/assets/logo.png")}
+    />
+</View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#F3F3FF",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
