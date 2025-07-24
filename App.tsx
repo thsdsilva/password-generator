80282d2cd780a2712bca2805e221f7806c9a411a
thsdsilva <thsds@pm.me>
2025-07-24 16:17:41 -0300

    Implement a slider and a generate password button ui and logic

--- App.tsx
+++ App.tsx
@@ -1,20 +1,84 @@
-import { StyleSheet, View, Text, Image } from 'react-native'
+import { useState } from 'react'
+import { StyleSheet, View, Text, Image, TouchableOpacity, Modal } from 'react-native'
+import Slider from '@react-native-community/slider'
+import { PasswordModal } from './src/components/modal'
 
 export default function App() {
-  return(
-<View style={ styles.container }>
-    <Image
-      source={require("./src/assets/logo.png")}
-    />
-</View>
+  const [size, setSize] = useState(10)
+  const [password, setpassword] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
+
+  function generatePassword() {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+    let generatedPassword = ""
+    for (let i = 0; i < size; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length)
+      generatedPassword += charset[randomIndex]
+    }
+    setpassword(generatedPassword)
+    setModalVisible(true)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("./src/assets/logo.png")}
+        style={styles.logo}
+      />
+
+      <Text style={styles.indicator}>{size} characters</Text>
+
+      <Slider
+        style={styles.slider}
+        minimumValue={6}
+        maximumValue={20}
+        maximumTrackTintColor='black'
+        minimumTrackTintColor='red'
+        value={size}
+        onValueChange={(value) => setSize(Math.round(value))}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
+        <Text style={styles.buttonText}>Generate password</Text>
+      </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType='fade' transparent={true}>
+        <PasswordModal/>
+      </Modal>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    backgroundColor: "#F3F3FF",
+    backgroundColor: '#F3F3FF',
     justifyContent: "center",
     alignItems: "center"
+  },
+  logo: {
+    marginBottom: 48,
+  },
+  indicator: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  slider: {
+    height: 50,
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+  },
+  button: {
+    backgroundColor: "#392DE9",
+    width: "80%",
+    height: 50,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
   }
 })
