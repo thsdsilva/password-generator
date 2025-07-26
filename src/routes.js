4d0bdd992ec85492c864b4f9fc9f48b0a8b4dd53
thsdsilva <thsds@pm.me>
2025-07-25 23:43:43 -0300

    Adjust tab icons position

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import { View } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "./pages/home";
 import { Passwords } from "./pages/passwords";
@@ -6,45 +7,40 @@ import { Ionicons } from '@expo/vector-icons';
 const Tab = createBottomTabNavigator();
 
 export function Routes() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen
-                name="home"
-                component={Home}
-                options={{
-                    tabBarShowLabel: false,
-                    headerShown: false,
-                    tabBarStyle: {
-                        height: 70,
-                    },
-                    tabBarIconStyle: {
-                        marginTop: 10,
-                    },
-                    tabBarIcon: ({ focused, size, color }) => {
-                        const iconName = focused ? 'home' : 'home-outline';
-                        return (<Ionicons name={iconName} size={size} color={color} />);
-                    },
-                }}
-            />
-            <Tab.Screen
-                name="passwords"
-                component={Passwords}
-                options={{
-                    tabBarShowLabel: false,
-                    headerShown: false,
-                    tabBarStyle: {
-                        height: 70,
-                    },
-                    tabBarIconStyle: {
-                        marginTop: 10,
-                    },
-                    tabBarIcon: ({ focused, size, color }) => {
-                        const iconName = focused ? 'lock-closed' : 'lock-closed-outline';
-                        return (<Ionicons name={iconName} size={size} color={color} />);
-                    },
-                }}
-            />
-        </Tab.Navigator>
-
-    )
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIconStyle: {
+          marginTop: 5,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => {
+            const iconName = focused ? 'home' : 'home-outline';
+            return (
+                <Ionicons name={iconName} size={size} color={color} />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="passwords"
+        component={Passwords}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => {
+            const iconName = focused ? 'lock-closed' : 'lock-closed-outline';
+            return (
+                <Ionicons name={iconName} size={size} color={color} />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
