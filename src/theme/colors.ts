79019c8d9561850a026a16f40ea457995dc553cc
thsdsilva <thsds@pm.me>
2025-08-17 00:41:09 -0300

    Create a color scheme to avoid repetition

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  primary: '#5568fe',
+  secondary: '#b0bfff',
+  terciary: '#f39c12',
+  text: '#4c5166',
+  muted: '#7b8097',
+  neutral: '#c0c4d8',
+  neutralVariant: '#9ea2b8',
+  border: '#d7dbf5',
+  borderVariant: '#b0b3c7',
+  background: '#ffffff',
+  backgroundVariant: '#f5f5f5',
+}
